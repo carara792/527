84f6a975875ee6082cbaa17ccb479bbce30db92c
Cara <carara.792@gmail.com>
2021-05-27 22:11:06 +0800

    first commit

--- main.tf
+++ main.tf
@@ -3,6 +3,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
+module "ec2_with_mandatory_tagging" {
+  source  = "github.com/cloudtp/hello-terraform-module"
+}
+
 data "aws_ssm_parameter" "cathay_ami" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
@@ -10,8 +14,10 @@ data "aws_ssm_parameter" "cathay_ami" {
 resource "aws_instance" "cathay-web" {
    ami = data. aws_ssm_parameter.cathay_ami.value
    instance_type = "t3.micro"
-
+    monitoring = true
    tags = {
        Name = "lab-server-1"
    }
+
+   user_data = fileexists("script.sh") ? file("script.sh") : null
 }
